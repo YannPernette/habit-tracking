6f0a1e13f221cf1abf81f3640072d727b5cf1b7c
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-11-25 15:04:53 +0100

    feat(button): création du composant
    - Structure dynamique
    - Styles (variants)

---
+++ src/components/Button.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+defineProps<{
+    disabled?: boolean;
+    variant?: "primary" | "outline";
+}>();
+
+</script>
+
+<template>
+    <button :class="{
+        'button': true,
+        '-outline': variant === 'outline',
+        '-disabled': disabled
+    }">
+        <slot></slot>
+    </button>
+</template>
+
+<style lang="scss">
+.button {
+    display: block;
+    font-weight: 500;
+    background: $primary;
+    color: $white;
+    border-radius: rem(48px);
+    padding: rem(16px) rem(32px);
+    cursor: pointer;
+    border: 1px solid $primary;
+    text-decoration: none;
+    text-align: center;
+    transition: all .3s ease-in-out;
+
+    &:hover {
+        background: $dark;
+    }
+
+    &.-outline {
+        background: transparent;
+        color: $primary;
+
+        &:hover {
+            color: $dark;
+        }
+    }
+
+    &.-nobg {
+        color: $primary;
+        background: transparent;
+        border: transparent;
+
+        &:hover {
+            background: $lightest;
+            border: $lightest;
+        }
+    }
+
+    &.-disabled {
+        color: $gray800;
+        background: $gray100;
+        border: $gray100;
+        cursor: not-allowed;
+
+        &.-outline {
+            background: transparent;
+            color: $gray400;
+            border: 1px solid $gray400;
+        }
+
+        &.-nobg {
+            background: transparent;
+            color: $gray400;
+            border: transparent;
+        }
+    }
+}
+</style>
