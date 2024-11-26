27b7e4935ff6084a0b534bedf38f0ea0d31d7853
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-11-26 08:49:18 +0100

    feat(input): création et style du composant Input (+ variants)

---
+++ src/components/Input.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+defineProps<{
+  placeholder: string;
+  type: string;
+  disabled?: boolean;
+}>();
+</script>
+
+
+<template>
+  <input :class="{
+    'input': true,
+    '-disabled': disabled
+  }" :placeholder="placeholder" :type="type" />
+</template>
+
+
+<style lang="scss">
+.input {
+  width: -webkit-fill-available;
+  color: $blackdarkest;
+  border: 1px solid $gray100;
+  padding: remTo(16px);
+  border-radius: remTo(8px);
+
+  &::placeholder {
+    color: $gray400;
+  }
+
+  &:focus-visible {
+    outline: solid 2px $primary;
+  }
+
+  &.-disabled {
+    color: $gray400;
+    border: 1px solid $gray400;
+    background-color: $gray100;
+    outline: none;
+    cursor: not-allowed;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Button from '@/components/Button.vue';
+import Input from '@/components/Input.vue';
 
 </script>
 
 <template>
   <main>
     <Button>Button Test</Button>
+    <Input placeholder="je suis le placeholder" type="email" />
   </main>
 </template>
