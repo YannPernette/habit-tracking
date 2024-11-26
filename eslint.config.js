c321d91bad513d3f1502af87f51971fe79cd7bd7
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-11-26 11:03:26 +0100

    bug(eslint): mise à jour des règles d'exception

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import pluginVue, { rules } from 'eslint-plugin-vue'
+import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
@@ -12,18 +12,14 @@ export default [
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-  {
-    rules: {
-      'vue/multi-word-component-names': [
-        'error',
-        {
-          ignores: [],
-        },
-      ],
-    },
-  },
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
   skipFormatting,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 ]
