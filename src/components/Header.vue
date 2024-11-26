825b6c9679ef2473aaf21ca76328be31380124b9
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-11-26 10:02:31 +0100

    feat(header): création du composant Header
    - Création composant Header
    - Importation du logo du site sous forme d'icône Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import LogoFull from './icon/LogoFull.vue';
+import Button from './Button.vue';
+</script>
+
+
+<template>
+  <header>
+    <div class="header">
+      <LogoFull class="header__logo" />
+      <ul class="header_navigation">
+        <li><RouterLink to="#">Accueil</RouterLink></li>
+        <li><RouterLink to="#">Mes habitudes</RouterLink></li>
+        <li><RouterLink to="#">Mon résumé mensuel</RouterLink></li>
+      </ul>
+      <Button>Se connecter</Button>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  &__logo {
+    width: 200px;
+    height: auto;
+  }
+
+  &__navigation {
+    display: flex;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Button from '@/components/Button.vue';
 import Input from '@/components/Input.vue';
+import Header from '@/components/Header.vue';
 
 </script>
 
 <template>
   <main>
+    <Header />
     <Button>Button Test</Button>
     <Input placeholder="je suis le placeholder" type="email" />
   </main>
