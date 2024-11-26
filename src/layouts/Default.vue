9448d3cfe3676f10f97755052b53c8f755c7362e
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-11-26 10:30:18 +0100

    feat(layout): création du Layout par défaut

---
+++ src/layouts/Default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="header">
+        <slot name="header"></slot>
+    </header>
+
+    <main>
+        <slot></slot>
+    </main>
+
+    <footer class="footer">
+        <slot name="footer"></slot>
+    </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,24 @@
 import Button from '@/components/Button.vue';
 import Input from '@/components/Input.vue';
 import Header from '@/components/Header.vue';
+import DefaultLayout from '@/layouts/Default.vue';
 
 </script>
 
 <template>
-  <main>
-    <Header />
-    <Button>Button Test</Button>
-    <Input placeholder="je suis le placeholder" type="email" />
-  </main>
+  <DefaultLayout>
+
+    <template #header>
+      <Header />
+    </template>
+
+    <template #aside>
+      <h1>Home</h1>
+    </template>
+
+    <template #footer>
+      <h1>Home</h1>
+    </template>
+
+  </DefaultLayout>
 </template>
