381830c48b836a7fdbd4267ac1f61ac2bfc1efeb
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-11-27 09:35:29 +0100

    feat(login): component LoginForm
    - composant LoginForm
    - icône Google (Vue)
    - style global h1

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,7 @@
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 import DefaultLayout from '@/layouts/Default.vue';
+import LoginForm from '@/components/LoginForm.vue';
 </script>
 
 
@@ -13,13 +14,13 @@ import DefaultLayout from '@/layouts/Default.vue';
     </template>
 
     <div class="login">
-
       <div class="login__background">
         <img class="login__backgroundImg"
           src="https://images.unsplash.com/photo-1483546363825-7ebf25fb7513?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="">
       </div>
 
+      <LoginForm />
     </div>
 
     <template #footer>
