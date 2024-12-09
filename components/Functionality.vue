0f8c243cba5271b0ec7de81d84489ef16c2b36dc
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-12-09 15:09:36 +0100

    feat(fonctionalities): style des composants fonctionnalités
    - style
    - import police Merriweather

--- components/Functionality.vue
+++ components/Functionality.vue
@@ -11,11 +11,30 @@ const { urlFor } = useSanityImage()
 
 
 <template>
-    <img v-if="image" class="" :src="urlFor(image)?.url()" alt="">
-    <h3 v-if="title">{{ title }}</h3>
-    <p v-if="subtitle">{{ subtitle }}</p>
-    <p v-if="text">{{ text }}</p>
+    <div class="functionality">
+        <img v-if="image" class="functionality__image" :src="urlFor(image)?.url()" alt="">
+        <h3 v-if="title" class="functionality__title">{{ title }}</h3>
+        <p v-if="subtitle" class="functionality__subtitle">{{ subtitle }}</p>
+        <p v-if="text">{{ text }}</p>
+    </div>
 </template>
 
 
-<style lang='scss'></style>
+<style lang='scss'>
+.functionality {
+    &__image {
+        width: 50px;
+    }
+
+    &__title {
+        font-family: 'Merriweather', serif;
+        margin-block-end: 0;
+    }
+
+    &__subtitle {
+        margin-block-start: remTo(5px);
+        font-style: italic;
+    }
+
+}
+</style>
