eb20ac3c4084601407f76d3230e1368bbf5a029a
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-12-11 16:55:03 +0100

    feat(cardHabit): style de la card + delete

--- pages/app/dashboard.vue
+++ pages/app/dashboard.vue
@@ -1,46 +1,16 @@
 <script setup lang="ts">
-const { data, refresh } = await useAsyncData('dashboard', async () =>
-    await useAPI('/dashboard', {
-        method: 'GET'
-    })
-)
-
-function onHabitCreated() {
-    console.log('Habitude créée avec succès')
-    refresh()
-}
-
-onMounted(() => {
-    $on('habit:created', onHabitCreated)
-})
-
-onBeforeUnmount(() => {
-    $off('habit:created', onHabitCreated)
-})
 </script>
 
 
 <template>
-    <!-- <ContainerHabits /> -->
-    <div style="margin-top: 100px;">
-        <!-- {{ data }} -->
-        <br><br>
-        <ul>
-            <li v-for="(habit, index) in data.globalHabits" :key="index">
-                <CardHabit v-bind="habit" />
-            </li>
-        </ul>
-        <br><br>
-        <ul>
-            <li v-for="(habit, index) in data.personalHabits" :key="index">
-                <CardHabit v-bind="habit" />
-            </li>
-        </ul>
-        <br><br>
+    <div class="mx-page" style="margin-top: 100px;">
+        <ContainerHabits which-habits="globalHabits" />
+        <ContainerHabits which-habits="personalHabits" />
 
-        <FormAddHabit @habit:created="onHabitCreated" />
+        <FormAddHabit />
+    </div>
 
-        <!-- <form @submit="deleteHabit">
+    <!-- <form @submit="deleteHabit">
             <div>
                 <label for="title">Titre</label>
                 <input id="title" v-model="title" type="title">
@@ -53,5 +23,4 @@ onBeforeUnmount(() => {
                 <button type="submit">ENVOYER</button>
             </div>
         </form> -->
-    </div>
 </template>
