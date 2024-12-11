0010f3f3f6f4b99d8d1b393ec978aa9dd1a21d2c
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-12-11 13:58:13 +0100

    feat(build): ajout Github Action "npm run build"
    - Github Action build
    - correction des variables d'environnement
    - fix eslint warning linebreak

--- pages/login.vue
+++ pages/login.vue
@@ -61,7 +61,8 @@ async function logout() {
     </div>
 
     <div class="login__background">
-      <img class="login__backgroundImg"
+      <img 
+        class="login__backgroundImg"
         src="https://images.unsplash.com/photo-1483546363825-7ebf25fb7513?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="">
     </div>
