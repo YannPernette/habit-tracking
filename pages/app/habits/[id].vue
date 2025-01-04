51e49cf6f1b59f5cf53cc3fa51516af9c73d3424
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2025-01-04 16:11:08 +0100

    feat(completed): ajout de la possibilité de compléter une habitude à une date donnée

--- pages/app/habits/[id].vue
+++ pages/app/habits/[id].vue
@@ -1,11 +1,26 @@
 <script setup lang='ts'>
 const { id } = useRoute().params;
 
+const completed = ref()
+const date = ref()
+
 const { data } = await useAsyncData('habit', async () =>
     await useAPI(`/habits/${id}`, {
         method: 'GET'
     })
 )
+
+async function completeHabit(event: Event) {
+    event.preventDefault()
+
+    await useAPI(`/tracking/${id}`, {
+        method: 'POST',
+        body: { completed: completed.value, date: date.value }
+    })
+
+    // emit('habit:created')
+    // $trigger('habit:created')
+}
 </script>
 
 
@@ -13,6 +28,23 @@ const { data } = await useAsyncData('habit', async () =>
 <div style="margin-top: 100px;">
     uoi
     <h1>{{ data.title }}</h1>
+    {{ completed }} {{ date }}
+
+    <div>
+        <form @submit="completeHabit($event)">
+            <div>
+                <label for="newTitle">Complétée</label>
+                <input id="newTitle" v-model="completed" type="checkbox" required>
+            </div>
+            <div>
+                <label for="newDescription">Date</label>
+                <input id="newDescription" v-model="date" type="date" required>
+            </div>
+            <div>
+                <button type="submit">ENVOYER</button>
+            </div>
+        </form>
+    </div>
 </div>
 </template>
 
