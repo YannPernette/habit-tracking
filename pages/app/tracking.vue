bb23c0911a69daff9e44a66731000b0873122f4d
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2025-01-05 20:34:53 +0100

    feat(style-tracking): style du tracking global des habitudes

--- pages/app/tracking.vue
+++ pages/app/tracking.vue
@@ -10,37 +10,52 @@ const { data, refresh } = await useAsyncData('tracking', async () =>
 )
 
 watch([selectedMonth, selectedYear], () => {
-  refresh();
+    refresh();
 });
 </script>
 
 
 <template>
-    <div style="margin-top: 140px;">
-        <div>
-            <label for="month-select">Mois</label>
-            <select id="month-select" v-model="selectedMonth">
-                <option v-for="month in 12" :key="month" :value="month.toString().padStart(2, '0')">
-                    {{ new Date(0, month - 1).toLocaleString("fr-FR", { month: "long" }) }}
-                </option>
-            </select>
+    <div class="tracking mx-page" style="margin-top: 140px;">
 
-            <label for="year-select">Année</label>
-            <select id="year-select" v-model="selectedYear">
-                <option v-for="year in [2022, 2023, 2024, 2025]" :key="year" :value="year">
-                    {{ year }}
-                </option>
-            </select>
+        <h1>Tracking global de toutes vos activités par mois et année</h1>
+
+        <div class="tracking__date">
+            <div class="tracking__option">
+                <label for="month-select">Mois</label>
+                <select id="month-select" v-model="selectedMonth" class="tracking__input">
+                    <option v-for="month in 12" :key="month" :value="month.toString().padStart(2, '0')">
+                        {{ new Date(0, month - 1).toLocaleString("fr-FR", { month: "long" }) }}
+                    </option>
+                </select>
+            </div>
+
+            <div class="tracking__option">
+                <label for="year-select">Année</label>
+                <select id="year-select" v-model="selectedYear" class="tracking__input">
+                    <option v-for="year in [2022, 2023, 2024, 2025]" :key="year" :value="year">
+                        {{ year }}
+                    </option>
+                </select>
+            </div>
         </div>
 
+        <h2>Vos habitudes :</h2>
+
         <ul>
-            <li v-for="(habit, index) in data.results" :key="index">
-                {{ habit.title }}
-                <p>Jours totaux : {{ habit.total_days }}</p>
-                <p>Taux de complétion</p>
-                <div class="tracking__completion" :style="{ width: habit.success_rate + '%' }" />
-                <p>{{ habit.success_rate }} %</p>
-                <p>Soit {{ habit.completed_days }} jours</p>
+            <li v-for="(habit, index) in data.results" :key="index" class="tracking__habits">
+                <div>
+                    <h3>{{ habit.title }}</h3>
+                    <p>Jours totaux : {{ habit.total_days }}</p>
+                    <p class="tracking__taux">Taux de complétion</p>
+                    <div class="tracking__progressbar">
+                        <div class="tracking__containerCompletion">
+                            <div class="tracking__completion" :style="{ width: habit.success_rate + '%' }" />
+                        </div>
+                        <p>{{ habit.success_rate }} %</p>
+                    </div>
+                    <p class="tracking__equivalence">Soit {{ habit.completed_days }} jours</p>
+                </div>
             </li>
         </ul>
     </div>
@@ -49,10 +64,70 @@ watch([selectedMonth, selectedYear], () => {
 
 <style lang='scss'>
 .tracking {
+    &__date {
+        display: flex;
+        justify-content: center;
+        gap: remTo(30px);
+        margin-top: remTo(40px);
+    }
+
+    &__option {
+        font-size: $title3;
+    }
+
+    &__input {
+        margin-left: remTo(10px);
+        border-radius: remTo(5px);
+        border: 1px solid $primary;
+        padding: remTo(5px);
+
+        &:focus {
+            outline: 1px $primary;
+        }
+    }
+
+    &__habits {
+        position: relative;
+        margin-bottom: 4rem;
+
+        &:not(:last-child)::after {
+            content: "";
+            height: 1px;
+            width: 100%;
+            position: absolute;
+            bottom: remTo(-30px);
+            background-color: $primary;
+        }
+    }
+
+    &__taux {
+        margin-bottom: 0;
+    }
+
+    &__progressbar {
+        display: flex;
+        gap: remTo(20px);
+        align-items: center;
+    }
+
+    &__containerCompletion {
+        width: 40%;
+        border: 1px solid $black;
+        border-radius: remTo(5px);
+        overflow: hidden;
+        height: fit-content;
+    }
+
     &__completion {
         background-color: $primary;
         height: 2rem;
         width: 0;
+    }
+
+    &__equivalence {
+        font-size: $smalltxt;
+        margin: 0;
+        color: $gray800;
     }
 }
 </style>
